67c5e9a0231b11bdf9317ec766fc3d3e7002bbab
KentStuff <thekentstuff@gmail.com>
2021-07-05 12:52:00 -0500

    Add project files.

---
+++ GPS/Forms/Form_Help.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using AgOpenGPS.Properties;
+using System.Windows.Forms;
+
+
+namespace AgOpenGPS
+{
+    public partial class Form_Help : Form
+    {
+        public Form_Help()
+        {
+            InitializeComponent();
+        }
+        private readonly FormGPS mf;
+        private string culture;
+        //constructor
+        public Form_Help(FormGPS _f)
+        {
+            mf = _f;
+            //if (mf != null)
+            //{
+            //    gl = _gl;
+            //}
+        }
+
+
+
+        private void Form_Help_Load(object sender, EventArgs e)
+        {
+            
+            bool notFound = false;
+            try
+            {
+                switch (Settings.Default.setF_culture)
+                {
+                    case "en":
+                        culture = "Manual.pdf";
+                        break;
+
+                    case "ru":
+                        culture = "Manual.ru.pdf";
+                        break;
+
+                    case "da":
+                        culture = "Manual.da.pdf";
+                        break;
+
+                    case "de":
+                        culture = "Manual.de.pdf";
+                        break;
+
+                    case "nl":
+                        culture = "Manual.nl.pdf";
+                        break;
+
+                    case "it":
+                        culture = "Manual.it.pdf";
+                        break;
+
+                    case "es":
+                        culture = "Manual.es.pdf";
+                        break;
+
+                    case "fr":
+                        culture = "Manual.fr.pdf";
+                        break;
+
+                    case "uk":
+                        culture = "Manual.uk.pdf";
+                        break;
+
+                    case "sk":
+                        culture = "Manual.sk.pdf";
+                        break;
+
+                    case "pl":
+                        culture = "Manual.pl.pdf";
+                        break;
+
+                    case "af":
+                        culture = "Manual.af.pdf";
+                        break;
+
+                    default:
+                        culture = "Manual.pdf";
+                        break;
+                }
+
+            }
+            catch
+            {
+                notFound = true;
+            }
+
+            if (notFound)
+            {
+                try
+                {
+                    culture = "Manual.pdf";
+                }
+                catch
+                {
+                    mf.TimedMessageBox(2000, "No File Found", "Can't Find Manual.pdf");
+                }
+            }
+
+
+            string directoryName = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+
+            HelpBrowser.Url = new Uri(directoryName+"\\"+culture);
+            
+           
+
+
+           
+        }
+
+      
+
+        //private void AOGHelpBtn_Click(object sender, EventArgs e)
+        //{
+        //    string directoryName = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+
+        //    HelpBrowser.Url = new Uri(directoryName + "\\" + culture);
+        //    AOGHelpBtn.Enabled = false;
+        //    treeHelpBtn.Enabled = !AOGHelpBtn.Enabled;
+
+        //}
+    }
+}
---
+++ GPS/Forms/Form_Help.designer.cs
@@ -0,0 +1,84 @@
+﻿namespace AgOpenGPS
+{
+    partial class Form_Help
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.HelpBrowser = new System.Windows.Forms.WebBrowser();
+            this.AOGHelpBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::AgOpenGPS.Properties.Resources.OK64;
+            this.button1.Location = new System.Drawing.Point(862, 629);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 70);
+            this.button1.TabIndex = 10;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // HelpBrowser
+            // 
+            this.HelpBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.HelpBrowser.Location = new System.Drawing.Point(5, 10);
+            this.HelpBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.HelpBrowser.Name = "HelpBrowser";
+            this.HelpBrowser.Size = new System.Drawing.Size(967, 613);
+            this.HelpBrowser.TabIndex = 11;
+           
+            // 
+            // Form_Help
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(984, 702);
+            this.Controls.Add(this.AOGHelpBtn);
+            this.Controls.Add(this.HelpBrowser);
+            this.Controls.Add(this.button1);
+            this.Name = "Form_Help";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "AOG-Tree Help";
+            this.Load += new System.EventHandler(this.Form_Help_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser HelpBrowser;
+        private System.Windows.Forms.Button AOGHelpBtn;
+    }
+}
